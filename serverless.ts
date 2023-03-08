ddef289eda19514febe6275c2a1125373b1ccda8
Leo Singer <leo.singer@ligo.org>
2023-03-08 10:27:51 -0500

    Support both OpenSearch Service and OpenSearch Serverless

---
+++ serverless.ts
@@ -0,0 +1,97 @@
+/*!
+ * Copyright © 2022 United States Government as represented by the Administrator
+ * of the National Aeronautics and Space Administration. No copyright is claimed
+ * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
+ *
+ * SPDX-License-Identifier: NASA-1.3
+ */
+
+export function cloudformationResources(collectionName: string) {
+  return {
+    OpenSearchServerlessCollection: {
+      Type: 'AWS::OpenSearchServerless::Collection',
+      DependsOn: [
+        'OpenSearchServerlessSecurityPolicyEncryption',
+        'OpenSearchServerlessSecurityPolicyNetwork',
+        'OpenSearchServerlessAccessPolicy',
+      ],
+      Properties: {
+        Name: collectionName,
+        Type: 'SEARCH',
+      },
+    },
+    OpenSearchServerlessSecurityPolicyEncryption: {
+      Type: 'AWS::OpenSearchServerless::SecurityPolicy',
+      Properties: {
+        Type: 'encryption',
+        Name: collectionName,
+        Policy: JSON.stringify({
+          Rules: [
+            {
+              ResourceType: 'collection',
+              Resource: [`collection/${collectionName}`],
+            },
+          ],
+          AWSOwnedKey: true,
+        }),
+      },
+    },
+    OpenSearchServerlessSecurityPolicyNetwork: {
+      Type: 'AWS::OpenSearchServerless::SecurityPolicy',
+      Properties: {
+        Type: 'network',
+        Name: collectionName,
+        Policy: JSON.stringify([
+          {
+            Rules: [
+              {
+                ResourceType: 'collection',
+                Resource: [`collection/${collectionName}`],
+              },
+            ],
+            AllowFromPublic: true,
+          },
+        ]),
+      },
+    },
+    OpenSearchServerlessAccessPolicy: {
+      Type: 'AWS::OpenSearchServerless::AccessPolicy',
+      Properties: {
+        Type: 'data',
+        Name: collectionName,
+        Policy: {
+          'Fn::Sub': [
+            JSON.stringify([
+              {
+                Rules: [
+                  {
+                    ResourceType: 'collection',
+                    Resource: [`collection/${collectionName}`],
+                    Permission: ['aoss:*'],
+                  },
+                  {
+                    ResourceType: 'index',
+                    Resource: [`index/${collectionName}/*`],
+                    Permission: ['aoss:*'],
+                  },
+                ],
+                // eslint-disable-next-line no-template-curly-in-string
+                Principal: ['${Role}'],
+              },
+            ]),
+            {
+              Role: { 'Fn::GetAtt': 'Role.Arn' },
+            },
+          ],
+        },
+      },
+    },
+  }
+}
+
+export const services = {
+  node: {
+    'Fn::GetAtt': 'OpenSearchServerlessCollection.CollectionEndpoint',
+  },
+  sig4service: 'aoss',
+}
---
+++ service.ts
@@ -0,0 +1,72 @@
+/*!
+ * Copyright © 2022 United States Government as represented by the Administrator
+ * of the National Aeronautics and Space Administration. No copyright is claimed
+ * in the United States under Title 17, U.S. Code. All Other Rights Reserved.
+ *
+ * SPDX-License-Identifier: NASA-1.3
+ */
+
+export function cloudformationResources({
+  availabilityZoneCount,
+  instanceCount,
+  instanceType,
+  volumeSize,
+}: Record<string, string | undefined>) {
+  if (!availabilityZoneCount)
+    throw new Error('availabilityZoneCount must be defined')
+  if (!instanceCount) throw new Error('instanceCount must be defined')
+  if (!instanceType) throw new Error('instanceType must be defined')
+  if (!volumeSize) throw new Error('volumeSize must be defined')
+
+  const AvailabilityZoneCount = parseInt(availabilityZoneCount)
+  const InstanceCount = parseInt(instanceCount)
+  const VolumeSize = parseInt(volumeSize)
+  const ZoneAwarenessEnabled = AvailabilityZoneCount > 1
+
+  return {
+    OpenSearchServiceDomain: {
+      Type: 'AWS::OpenSearchService::Domain',
+      Properties: {
+        AccessPolicies: {
+          Version: '2012-10-17',
+          Statement: [
+            {
+              Effect: 'Allow',
+              Action: 'es:ESHttp*',
+              Resource: '*',
+              Principal: { AWS: { 'Fn::GetAtt': 'Role.Arn' } },
+            },
+          ],
+        },
+        ClusterConfig: {
+          InstanceType: instanceType,
+          InstanceCount,
+          ZoneAwarenessEnabled,
+          ...(ZoneAwarenessEnabled && {
+            ZoneAwarenessConfig: {
+              AvailabilityZoneCount,
+            },
+          }),
+        },
+        DomainEndpointOptions: { EnforceHTTPS: true },
+        EBSOptions: { EBSEnabled: true, VolumeSize },
+        EncryptionAtRestOptions: { Enabled: true },
+        NodeToNodeEncryptionOptions: { Enabled: true },
+      },
+    },
+  }
+}
+
+export const services = {
+  node: {
+    'Fn::Sub': [
+      'https://${DomainEndpoint}',
+      {
+        DomainEndpoint: {
+          'Fn::GetAtt': 'OpenSearchServiceDomain.DomainEndpoint',
+        },
+      },
+    ],
+  },
+  sig4service: 'es',
+}
