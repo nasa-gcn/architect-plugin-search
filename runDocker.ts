1fb60d6219c682b30c98097e75e998d28a15e4a4
Leo Singer <leo.singer@ligo.org>
2025-04-01 17:36:45 -0400

    Separate out code for starting and stopping Docker container
    Separate out the code for starting and stopping the Docker
    container and supervising it with a subprocess so that we can test
    it separately from Architect itself.

--- runDocker.ts
+++ runDocker.ts
@@ -6,25 +6,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import Dockerode from 'dockerode'
-import { fork } from 'node:child_process'
 import { type SearchEngineLauncherFunction } from './run.js'
-import { promisify } from 'node:util'
+import { launchDockerSubprocess } from './docker.js'
 
-const [, , command, jsonifiedArgs] = process.argv
-
-if (command === 'launch-docker-subprocess') {
-  const { dataDir, logsDir, engine, port, options } = JSON.parse(jsonifiedArgs)
-  const Image =
-    engine === 'elasticsearch'
-      ? 'elastic/elasticsearch:8.6.2'
-      : 'opensearchproject/opensearch:2.11.0'
-  console.log('Launching Docker container', Image)
-  const docker = new Dockerode()
-
-  await promisify(docker.modem.followProgress)(await docker.pull(Image))
-
-  const container = await docker.createContainer({
+export const launchDocker: SearchEngineLauncherFunction = async ({
+  dataDir,
+  logsDir,
+  engine,
+  port,
+  options,
+}) =>
+  launchDockerSubprocess({
     Env: [...options, 'path.data=/var/lib/search', 'path.logs=/var/log/search'],
     HostConfig: {
       AutoRemove: true,
@@ -36,54 +28,8 @@ if (command === 'launch-docker-subprocess') {
         [`${port}/tcp`]: [{ HostIP: '127.0.0.1', HostPort: `${port}` }],
       },
     },
-    Image,
+    Image:
+      engine === 'elasticsearch'
+        ? 'elastic/elasticsearch:8.6.2'
+        : 'opensearchproject/opensearch:2.11.0',
   })
-  const stream = await container.attach({ stream: true, stderr: true })
-  stream.pipe(process.stderr)
-  await container.start()
-
-  const signals = ['message', 'SIGTERM', 'SIGINT']
-  signals.forEach((signal) => {
-    process.on(signal, async () => {
-      await container.kill()
-    })
-  })
-
-  await container.wait()
-  process.exit()
-}
-
-export const launchDocker: SearchEngineLauncherFunction = async ({
-  dataDir,
-  logsDir,
-  engine,
-  port,
-  options,
-}) => {
-  const argv = {
-    dataDir,
-    logsDir,
-    engine,
-    port,
-    options,
-  }
-  const subprocess = fork(new URL(import.meta.url), [
-    'launch-docker-subprocess',
-    JSON.stringify(argv),
-  ])
-
-  return {
-    async kill() {
-      console.log('Killing Docker container')
-      subprocess.send({ action: 'kill' })
-    },
-    async waitUntilStopped() {
-      return new Promise((resolve) => {
-        subprocess.on('exit', () => {
-          console.log('Docker container exited')
-          resolve()
-        })
-      })
-    },
-  }
-}
