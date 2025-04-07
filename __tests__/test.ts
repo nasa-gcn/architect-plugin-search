cde4a88e76e5ce7776e39ce296e6c1623469c9fe
Leo Singer <leo.singer@ligo.org>
2025-04-07 10:18:50 -0400

    Remove per-signal parametrization from tests

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -5,65 +5,61 @@ import { fileURLToPath } from 'node:url'
 import { ExecaError, execa } from 'execa'
 import { fetchRetry, sleep } from '@nasa-gcn/architect-plugin-utils'
 
-const signals = ['SIGTERM'] as const
 const engines = ['elasticsearch', 'opensearch']
 
 engines.forEach((engine) =>
-  signals.forEach((signal) => {
-    describe(`${engine} stops when sent signal ${signal}`, () => {
-      let process: ReturnType<typeof execa> | undefined
+  describe(`${engine} stops on Ctrl-C`, () => {
+    let process: ReturnType<typeof execa> | undefined
 
-      beforeEach(async () => {
-        // FIXME: replace with import.meta.resolve once it is stable in Node.js
-        const cwd = join(dirname(fileURLToPath(import.meta.url)), engine)
+    beforeEach(async () => {
+      // FIXME: replace with import.meta.resolve once it is stable in Node.js
+      const cwd = join(dirname(fileURLToPath(import.meta.url)), engine)
 
-        process = execa('arc', ['sandbox'], {
-          cwd,
-          preferLocal: true,
-          forceKillAfterDelay: false,
-          stdin: 'pipe',
-          stderr: 'inherit',
-          stdout: ['inherit', 'pipe'],
-        })
-
-        return new Promise<void>((resolve) => {
-          process?.stdout?.on('data', (chunk) => {
-            if (chunk.includes('Ran Sandbox startup plugin in')) resolve()
-          })
-        })
+      process = execa('arc', ['sandbox'], {
+        cwd,
+        preferLocal: true,
+        forceKillAfterDelay: false,
+        stdin: 'pipe',
+        stderr: 'inherit',
+        stdout: ['inherit', 'pipe'],
       })
 
-      afterEach(async () => {
-        if (process) {
-          // Type Ctrl-C into Architect's stdin
-          process.stdin?.write('\u0003')
-          assert.ok(process.kill(signal))
-          // Make sure arc sandbox is dead
-          try {
-            await process
-          } catch (e) {
-            if (!(e instanceof ExecaError)) throw e
-          }
-          // Give subprocesses some time to die
-          await sleep(1000)
+      return new Promise<void>((resolve) => {
+        process?.stdout?.on('data', (chunk) => {
+          if (chunk.includes('Ran Sandbox startup plugin in')) resolve()
+        })
+      })
+    })
 
-          // Make sure that arc sandbox and opensearch/elasticseasrch are both
-          // down and not responding to HTTP requests any more
-          for (const port of [3333, 9200]) {
-            await assert.rejects(
-              fetch(`http://localhost:${port}/`),
-              TypeError,
-              `port ${port} must be closed`
-            )
-          }
+    afterEach(async () => {
+      if (process) {
+        // Type Ctrl-C into Architect's stdin
+        process.stdin?.write('\u0003')
+        // Make sure arc sandbox is dead
+        try {
+          await process
+        } catch (e) {
+          if (!(e instanceof ExecaError)) throw e
         }
-      })
+        // Give subprocesses some time to die
+        await sleep(1000)
 
-      test('connection was alive', async () => {
-        const response = await fetchRetry('http://localhost:3333/')
-        const result = await response.json()
-        assert.deepStrictEqual(result?.meta.connection.status, 'alive')
-      })
+        // Make sure that arc sandbox and opensearch/elasticseasrch are both
+        // down and not responding to HTTP requests any more
+        for (const port of [3333, 9200]) {
+          await assert.rejects(
+            fetch(`http://localhost:${port}/`),
+            TypeError,
+            `port ${port} must be closed`
+          )
+        }
+      }
+    })
+
+    test('connection was alive', async () => {
+      const response = await fetchRetry('http://localhost:3333/')
+      const result = await response.json()
+      assert.deepStrictEqual(result?.meta.connection.status, 'alive')
     })
   })
 )
